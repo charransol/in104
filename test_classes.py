fe45d6fa592a4c8a7deff3616cdec96f0a0f1c85
Charransol Louise <louise.charransol@ensta-paristech.fr>
2019-03-25 10:33:29 +0100

    pas d inspi 1

--- test_classes.py
+++ test_classes.py
@@ -6,28 +6,26 @@ class Vehicule():
         self.couleur = couleur
     def peinture(self,nouvelle_couleur):
         self.couleur=nouvelle_couleur
-    def get_voiture(self):
-        print ('le vehicule est un',self.type,self.modele, self.marque, self.couleur)
 
 class Moto(Vehicule):
     def __init__ (self,marque,modele,couleur):
         Vehicule.__init__(self,'moto',marque,modele,couleur)
         
-
+    def get_moto(self):
+        print ('le vehicule est une moto',self.modele, self.marque, self.couleur)
 
 class Voiture(Vehicule):
-    def __init__ (self,marque,modele,couleur):
+    def __init__ (self,marque,modele,couleur,rm,d_ou_e):
         Vehicule.__init__(self,'voiture',marque,modele,couleur)
-
-class Bus(Vehicule):
-    def __init__ (self,marque,modele,couleur):
-        Vehicule.__init__(self,'bus',marque,modele,couleur)
-
+        self.rm=rm
+        self.moteur=d_ou_e
+    def get_voiture(self):
+        print ('le vehicule est une voiture',self.modele, self.marque, self.couleur,self.rm,self.moteur)
 
 #voiture=Vehicule('voiture','citoen','c3','bleue')
 #voiture.get_voiture()
 #voiture.peinture('noir')
 #voiture.get_voiture()
 
-voiture=Voiture('citoen','c3','bleue')
+voiture=Voiture('citoen','c3','bleue',2,'e')
 voiture.get_voiture()

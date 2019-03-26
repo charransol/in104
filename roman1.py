0bdb8c2cc64d210fa8cdbd15f66a8863757fd84e
Charransol Louise <louise.charransol@ensta-paristech.fr>
2019-03-26 17:08:11 +0100

    times new

--- roman1.py
+++ roman1.py
@@ -2,9 +2,15 @@ def to_roman (integer):
     result=''
     if int(integer)!=integer:
         break()
-    while integer>=1000:
-        integer=integer-1000
-        result=result+'M'
-    while 100<=integer<=399:
-        
-        
+    
+    if 5<=integer<=8:
+        result=result+b
+        integer=integer-5
+    while 1<=integer<=3:
+        result=result+a
+        integer=integer-1
+    if integer==4:
+        result=result+a+b
+    if integer==9:
+        result=result+a+c
+    

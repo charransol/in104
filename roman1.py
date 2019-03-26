de11328c9f3db75b53ec936c5c89ed5492ec8427
Lemaitre Titouan <titouanlemaitre@gmail.com>
2019-03-26 17:31:03 +0100

    ca marche

--- roman1.py
+++ roman1.py
@@ -1,7 +1,17 @@
-def to_roman (integer):
+def pretraitement (n):
+    tab=[0,0,0,0]
+    i=0
+    while i<=3:
+        tab[i]=n//10**(3-i)
+        n=n%10**(3-i)
+        i=i+1
+    return(tab)
+    
+
+def to_lettre (integer,a,b,c):
     result=''
-    if int(integer)!=integer:
-        break()
+    #if int(integer)!=integer:
+     #   break()
     
     if 5<=integer<=8:
         result=result+b
@@ -13,4 +23,35 @@ def to_roman (integer):
         result=result+a+b
     if integer==9:
         result=result+a+c
+    return(result)
     
+def to_roman(n):
+    result=''
+    tab=pretraitement(n)
+    i=0
+    while i<=3:
+        if i==0:
+            a='M'
+            b=''
+            c=''
+        
+        if i==1:
+            a='C'
+            b='D'
+            c='M'
+        if i==2:
+            a='X'
+            b='L'
+            c='C'
+        if i==3:
+            a='I'
+            b='V'
+            c='X'
+        
+        integer=tab[i]
+        result=result+to_lettre(integer,a,b,c)
+        i+=1
+    return(result)
+    
+        
+        
